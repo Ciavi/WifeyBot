0a47dd9f13a9487b1816bdae7d771b6081d40ed1
Ciavi <13613469+Ciavi@users.noreply.github.com>
2025-07-07 12:55:14 +0200

    Migrating to pydot+graphviz

--- data/interface.py
+++ data/interface.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 import discord
 import matplotlib.pyplot as plt
 import networkx
+import pydot
 
 from data.models import User
 
@@ -94,129 +95,6 @@ async def u_emancipate(invoker: discord.User | discord.Member):
     await d_target.parent.disconnect(d_invoker)
 
 
-async def u_graph(target: discord.User | discord.Member):
-    d_target: User = await update_or_create_user(user_id=target.id, user_name=target.name)
-    t_partners: list[User] = await d_target.partners.all()
-    t_children: list[User] = await d_target.children.all()
-    t_parent: User = await d_target.parent.single()
-
-    graph = networkx.MultiDiGraph()
-    graph.add_node(d_target.user_name, color="#F7374F", label_color="#FFF")
-
-    if t_parent is not None:
-        graph.add_node(t_parent.user_name, color="#547b59", label_color="#FFF")
-        graph.add_edge(
-            d_target.user_name,
-            t_parent.user_name,
-            relationship="c.",
-            color="#55cfe2",
-            label_color="#55cfe2"
-        )
-
-    for t_partner in t_partners:
-        if t_partner.user_name == d_target.user_name:
-            graph.add_edge(
-                d_target.user_name,
-                t_partner.user_name,
-                relationship="m.",
-                color="#8ccd95",
-                label_color="#8ccd95"
-            )
-            continue
-
-        graph.add_node(t_partner.user_name, color="#547b59", label_color="#FFF")
-        graph.add_edge(
-            d_target.user_name,
-            t_partner.user_name,
-            relationship="m.",
-            color="#8ccd95",
-            label_color="#8ccd95"
-        )
-
-        partners: list[User] = await t_partner.partners.all()
-        children: list[User] = await t_partner.children.all()
-
-        for partner in partners:
-            if partner.user_name == d_target.user_name:
-                graph.add_edge(
-                    t_partner.user_name,
-                    d_target.user_name,
-                    relationship="m.",
-                    color="#8ccd95",
-                    label_color="#8ccd95"
-                )
-                continue
-
-            if t_parent is not None and partner.user_name == t_parent.user_name:
-                graph.add_edge(
-                    t_partner.user_name,
-                    partner.user_name,
-                    relationship="m.",
-                    color="#8ccd95",
-                    label_color="#8ccd95"
-                )
-                continue
-
-            graph.add_node(partner.user_name, color="#547b59", label_color="#FFF")
-            graph.add_edge(
-                t_partner.user_name,
-                partner.user_name,
-                relationship="m.",
-                color="#8ccd95",
-                label_color="#8ccd95"
-            )
-
-        for child in children:
-            graph.add_node(child.user_name, color="#8d6c79", label_color="#FFF")
-            graph.add_edge(
-                child.user_name,
-                t_partner.user_name,
-                relationship="c.",
-                color="#ecb5ca",
-                label_color="#ecb5ca"
-            )
-
-    for t_child in t_children:
-        graph.add_node(t_child.user_name, color="#8d6c79", label_color="#FFF")
-        graph.add_edge(
-            t_child.user_name,
-            d_target.user_name,
-            relationship="c.",
-            color="#ecb5ca",
-            label_color="#ecb5ca"
-        )
-
-    node_colors = list(networkx.get_node_attributes(graph, "color").values())
-    node_colors_label = list(networkx.get_node_attributes(graph, "label_color").values())
-    edge_colors = list(networkx.get_edge_attributes(graph, "color").values())
-    edge_colors_label = list(networkx.get_edge_attributes(graph, "label_color").values())
-
-    pos = networkx.spring_layout(G=graph, k=3, scale=7.50, seed=42)
-
-    ax = plt.gca()
-    ax.set_facecolor("#00000000")
-    ax.set_frame_on(False)
-    ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-
-    for i, node in enumerate(graph.nodes()):
-        networkx.draw_networkx_nodes(graph, pos, nodelist=[node], node_color=node_colors[i])
-        networkx.draw_networkx_labels(graph, pos, labels={node:node}, font_color=node_colors_label[i], font_size=8)
-
-    i = 0
-    for u, v, data in graph.edges(data=True):
-        rel = data.get("relationship", "")
-        networkx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], connectionstyle="arc3,rad=0.075", width=0.5, arrowstyle="->", arrowsize=7, edge_color=edge_colors[i])
-        networkx.draw_networkx_edge_labels(graph, pos, edge_labels={(u, v): rel}, font_color=edge_colors_label[i], font_size=4, bbox={ "fc": "#00000000", "ec": "#00000000", "boxstyle": "circle" })
-        i += 1
-
-    letters = string.ascii_lowercase
-    uid = ''.join(random.choice(letters) for _ in range(12))
-
-    plt.savefig(f"tmp/{target.id}-{uid}.png", format="png", dpi=300, bbox_inches="tight", facecolor="#00000000")
-    plt.close("all")
-    return uid
-
-
 class NodeType(enum.IntEnum):
     SELF = 0
     PARENT = 1
@@ -242,7 +120,7 @@ class Edge:
     type: EdgeType
 
 
-async def u_newgraph(target: discord.User | discord.Member):
+async def u_graph(target: discord.User | discord.Member):
     node_table: dict[str, Node] = {}
     edge_table: dict[tuple[str, str], Edge] = {}
 
@@ -293,37 +171,49 @@ async def u_newgraph(target: discord.User | discord.Member):
         add_edge(Edge(t_child.user_name, d_target.user_name, EdgeType.CHILD))
 
     graph = networkx.MultiDiGraph()
-    for label, _ in node_table.items():
-        graph.add_node(label)
-    for (f, t), _ in edge_table.items():
-        graph.add_edge(f, t)
-
-    pos = networkx.spring_layout(G=graph, k=3, scale=7.50, seed=42)
-
-    ax = plt.gca()
-    ax.set_facecolor("#00000000")
-    ax.set_frame_on(False)
-    ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-
-    print(list(graph.nodes))
-    print(list(graph.edges))
-
     for label, node in node_table.items():
-        color = "#FFB7B2" if node.type == NodeType.SELF else ("#A3C4F3"
-                                                              if node.type == NodeType.PARENT else ("#CBAACB" if node.type == NodeType.PARTNER else "#B5EAD7"))
-        networkx.draw_networkx_nodes(graph, pos, nodelist=[label], node_color=color)
-        networkx.draw_networkx_labels(graph, pos, labels={label:node.label}, font_color="#1B1C22", font_size=8)
-
+        graph.add_node(label, type=node.type.name)
     for (f, t), edge in edge_table.items():
-        color = "#CBAACB" if edge.type == EdgeType.PARTNER else "#B5EAD7"
-        direction = "<->" if edge.type == EdgeType.PARTNER else "->"
-        label = "m." if edge.type == EdgeType.PARTNER else "c."
-        networkx.draw_networkx_edges(graph, pos, edgelist=[(f, t)], connectionstyle=f"arc3,rad={0.075 if f != t else 0.5}", width=0.5, arrowstyle=direction, arrowsize=7, edge_color=color)
-        networkx.draw_networkx_edge_labels(graph, pos, edge_labels={(f, t): label}, font_color=color, font_size=4, bbox={ "fc": "#00000000", "ec": "#00000000", "boxstyle": "circle" })
+        graph.add_edge(f, t, type=edge.type.name)
+
+    node_styles = {
+        NodeType.SELF.name: { "fillcolor": "#ffb7b2", "fontcolor": "#1b1c22" },
+        NodeType.PARENT.name: { "fillcolor": "#a3c4f3", "fontcolor": "#1b1c22" },
+        NodeType.PARTNER.name: { "fillcolor": "#cbaacb", "fontcolor": "#1b1c22" },
+        NodeType.CHILD.name: { "fillcolor": "#b5ead7", "fontcolor": "#1b1c22" }
+    }
+
+    edge_styles = {
+        EdgeType.PARTNER.name: { "color": "#cbaacb", "fontcolor": "#cbaacb" },
+        EdgeType.CHILD.name: { "color": "#b5ead7", "fontcolor": "#b5ead7" }
+    }
+
+    pdot = networkx.drawing.nx_pydot.to_pydot(graph)
+
+    for node in pdot.get_nodes():
+        node_name = node.get_name().strip()
+        node_type = graph.nodes[node_name].get("type", "SELF")
+        style = node_styles.get(node_type)
+
+        node.set("shape", "hexagon")
+        node.set("style", "filled")
+        node.set("fillcolor", style.get("fillcolor"))
+        node.set("fontcolor", style.get("fontcolor"))
+        node.set("fontsize", 8.0)
+
+    for edge in pdot.get_edges():
+        edge_type = edge.get("type")
+        style = edge_styles.get(edge_type)
+
+        edge.set("dir", "both" if edge_type == EdgeType.PARTNER.name else "forward")
+        edge.set("label", "m." if edge_type == EdgeType.PARTNER.name else "c.")
+        edge.set("color", style.get("color"))
+        edge.set("fontcolor", style.get("fontcolor"))
+        edge.set("fontsize", 4.0)
+
 
     letters = string.ascii_lowercase
     uid = ''.join(random.choice(letters) for _ in range(12))
 
-    plt.savefig(f"tmp/{target.id}-{uid}.png", format="png", dpi=300, bbox_inches="tight", facecolor="#00000000")
-    plt.close("all")
+    pdot.write_png(f"tmp/{target.id}-{uid}.png")
     return uid
